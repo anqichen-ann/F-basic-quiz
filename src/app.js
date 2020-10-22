e5d62831828178ef54b2667ebb8d0f42954a3d50
annie <anqi.chen@thoughtworks.com>
2020-10-22 09:52:22 +0800

    [anqi] fix renderUserInfo & renderEduInfo

--- src/app.js
+++ src/app.js
@@ -1,2 +1,33 @@
 import "./style/index.scss";
+import {getUserInfo, getEduInfo} from './index';
 
+
+  let pathname = "/users/1";
+
+  async function renderUserInfo(pathname){
+    let userInfo = await getUserInfo(pathname);
+    $("img").attr("src", userInfo.avatar);
+    $(".title").html(
+      `MY NAME IS ${userInfo.name} ${userInfo.age}YO AND THIS IS MY RESUME/CV`
+    );
+    $(".desc").html(userInfo.description);
+  }
+
+  async function renderEduInfo(pathname){
+
+    let eduList = await getEduInfo(pathname);
+    eduList.forEach((edu) => {
+      $(".edu-list").append(`
+    <li>
+      <p class="year">${edu.year}</p>
+      <div class="edu-desc">
+        <h4>${edu.title}</h4>
+        <p >${edu.description}</p>
+        </div>
+    </li>`);
+    });
+  }
+  renderUserInfo(pathname);
+  renderEduInfo(pathname);
+
+  export {renderUserInfo, renderEduInfo};
--- src/index.js
+++ src/index.js
@@ -1,44 +1,14 @@
 import axios from "axios";
 
-
-$(function(){
-  let pathname = "/users/1"
-  getUserInfo(pathname);
-  getEduInfo(pathname);
-})
-
  function getUserInfo(pathname){
  const url1 = `http://localhost:8080${pathname}`;
-
-        axios.get(url1).then(
-          (response) => {
-            let user = response.data;
-            $("img").attr("src", user.avatar);
-            $(".title").html(
-              `MY NAME IS ${user.name} ${user.age}YO AND THIS IS MY RESUME/CV`
-            );
-            $(".desc").html(user.description);
-          });
+    return    axios.get(url1).then(
+          (response) => response.data);
 }
 
   function getEduInfo(pathname){
   const url2 = `http://localhost:8080${pathname}/educations`;
-
-        axios.get(url2).then(
-        (response) => {
-          let eduList = response.data;
-            eduList.forEach((edu) => {
-              $(".edu-list").append(`
-            <li>
-              <p class="year">${edu.year}</p>
-              <div class="edu-desc">
-                <h4>${edu.title}</h4>
-                <p >${edu.description}</p>
-                </div>
-            </li>`);
-            });
-        });
-
-      }
+    return  axios.get(url2).then(
+        (response) => response.data);}
 
 export {getUserInfo, getEduInfo};
